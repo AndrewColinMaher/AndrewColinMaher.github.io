30b3a13606776e8680035fa487657fcebf6e330a
AndrewColinMaher <colinmaher1710@yahoo.ie>
2020-11-05 14:04:38 +0000

    added javascript fle with code targeting modals in library

---
+++ script.js
@@ -0,0 +1,29 @@
+$("#modal1").on("hidden.bs.modal", function (e) {
+  // do something...
+  $("#modal1 iframe").attr("src", $("#modal1 iframe").attr("src"));
+});
+
+$("#modal6").on("hidden.bs.modal", function (e) {
+  // do something...
+  $("#modal6 iframe").attr("src", $("#modal6 iframe").attr("src"));
+});
+
+$("#modal4").on("hidden.bs.modal", function (e) {
+  // do something...
+  $("#modal4 iframe").attr("src", $("#modal4 iframe").attr("src"));
+});
+
+$("#modal3").on("hidden.bs.modal", function (e) {
+  // do something...
+  $("#modal3 iframe").attr("src", $("#modal3 iframe").attr("src"));
+});
+
+$("#modal9").on("hidden.bs.modal", function (e) {
+  // do something...
+  $("#modal9 iframe").attr("src", $("#modal9 iframe").attr("src"));
+});
+
+$("#modal11").on("hidden.bs.modal", function (e) {
+  // do something...
+  $("#modal11 iframe").attr("src", $("#modal11 iframe").attr("src"));
+});
